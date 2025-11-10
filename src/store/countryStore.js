bc08382c364c8e8f3e6fddf5d0a3d99c51a75a80
test <test@test.com>
2025-11-10 18:50:27 +0330

    fix country flags path

--- src/store/countryStore.js
+++ src/store/countryStore.js
@@ -3,10 +3,10 @@ import { create } from 'zustand';
 
 // Define our initial data. The flagUrl points to the /public folder.
 const countriesData = [
-  { id: 'PT', name: 'Portugal', flagUrl: '/flags/portugal.svg' },
-  { id: 'SE', name: 'Sweden', flagUrl: '/flags/sweden.svg' },
-  { id: 'FR', name: 'France', flagUrl: '/flags/france.svg' },
-  { id: 'BE', name: 'Belgium', flagUrl: '/flags/belgium.svg' },
+  { id: 'PT', name: 'Portugal', flagUrl: 'flags/portugal.svg' },
+  { id: 'SE', name: 'Sweden', flagUrl: 'flags/sweden.svg' },
+  { id: 'FR', name: 'France', flagUrl: 'flags/france.svg' },
+  { id: 'BE', name: 'Belgium', flagUrl: 'flags/belgium.svg' },
 ];
 
 const useCountryStore = create((set) => ({
